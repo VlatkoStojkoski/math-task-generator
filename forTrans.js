29eccd48ac40008e3e9ee084a0e12ae51e1abc5a
Vlatko Stojkoski <vlatko@stojkoski.com>
2020-03-25 00:10:54 +0100

    .gitignore update

--- forTrans.js
+++
@@ -1,9 +0,0 @@
-if (pageNum==0) {
-    tA[19].innerText = 'About This Project';
-    tA[20][0].innerText = 'This project was made regarding the COVID-19 situation.';
-    tA[20][1].innerText = 'Young kids can solve randomly generated tasks without bothering their parents to write tasks for them, so that parents can focus more on their work from home.';
-    tA[20][2].innerText = 'This website motivates kids to solve more tasks by higlighting the correct results in green.';
-    tA[21].innerText = 'About Me';
-    tA[22][0].innerText = 'My name is Vlatko, I am a web designer/developer based in Macedonia. I like making websites partly inspired by minimalism. I love what I do, and adore the creative process. It is addicting and can give never ending possibilities.';
-    tA[22][1].innerText = 'Developing has always been my favourite, but lately I\'ve been doing a lot of design too. I thoroughly enjoy doing so.';
-} else 
